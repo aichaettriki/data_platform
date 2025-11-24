38faeeef1708753188a7dbd2a41e723d0398e5a3
Nour Guelmami <Nourelhouda.guelmami@wimbeetech.com>
2025-11-24 17:24:04 +0100

    add minio partitionning

--- spark/jobs/etl_equipes.py
+++ spark/jobs/etl_equipes.py
@@ -1,5 +1,8 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, concat_ws, current_timestamp
+from pyspark.sql.functions import year, month, dayofmonth, current_timestamp
+from datetime import datetime
+
 
 def create_spark_session():
     spark = (
@@ -24,22 +27,51 @@ def read_raw_csv(spark, raw_path):
     return df
 
 
-def clean_and_write(df, transformed_path):
-    transformed_path = "s3a://transformed/equipe_spark.csv"
+from pyspark.sql.functions import year, month, dayofmonth, current_timestamp
+
+def clean_and_write(df, transformed_root_path, dataset_name="equipe_spark"):
+    now = current_timestamp()
+
     df_clean = df.dropDuplicates()
-    df_clean.coalesce(1).write.csv(transformed_path, mode="overwrite", header=True)
+
+    # date extraction
+    annee = df_clean.select(year(now)).first()[0]
+    mois = df_clean.select(month(now)).first()[0]
+    jour = df_clean.select(dayofmonth(now)).first()[0]
+
+    # ajouter le nom du dataset dans le chemin
+    output_path = f"{transformed_root_path}/{dataset_name}/{annee}/{mois}/{jour}/"
+
+    # Écriture
+    df_clean.coalesce(1).write \
+        .mode("overwrite") \
+        .option("header", True) \
+        .csv(output_path)
+
     return df_clean
 
+def add_timestamp_and_write(df_clean, refined_root_path):
+    now = current_timestamp()
 
-def add_timestamp_and_write(df_clean, refined_path):
-    refined_path = refined_path
     df_refined = (
         df_clean
         .withColumn("fullname", concat_ws(" ", col("nom"), col("prenom")))
         .withColumn("timestamp", current_timestamp())
     )
-    df_refined.coalesce(1).write.csv(refined_path, mode="overwrite", header=True)
-    return df_refined
+
+    # Extraire la date d’exécution
+    annee = df_clean.select(year(now)).first()[0]
+    mois = df_clean.select(month(now)).first()[0]
+    jour = df_clean.select(dayofmonth(now)).first()[0]
+
+    output_path = f"{refined_root_path}/{annee}/{mois}/{jour}/"
+
+    df_refined.coalesce(1).write \
+        .mode("overwrite") \
+        .option("header", True) \
+        .csv(output_path)
+
+    return output_path
 
 
 def write_to_postgres(df_refined, jdbc_url):
@@ -64,16 +96,21 @@ if __name__ == "__main__":
 
     elif action == "clean_and_transformed":
         df = read_raw_csv(spark, "s3a://raw/equipe.csv")
-        clean_and_write(df, "s3a://transformed/equipe_spark.csv")
+        clean_and_write(df, "s3a://transformed/equipe_spark")
 
     elif action == "add_timestamp_refined":
         df = read_raw_csv(spark, "s3a://raw/equipe.csv")
-        df_clean = clean_and_write(df, "s3a://transformed/equipe_spark.csv")
-        add_timestamp_and_write(df_clean, "s3a://refined/equipe_spark.csv")
+        df_clean = clean_and_write(df, "s3a://transformed/equipe_spark")
+        add_timestamp_and_write(df_clean, "s3a://refined/equipe_spark")
 
     elif action == "write_postgres":
-        df = spark.read.csv("s3a://refined/equipe_spark.csv", header=True, inferSchema=True)
+        today = datetime.today()
+        df = spark.read.csv(f"s3a://refined/equipe_spark/{today.year}/{today.month}/{today.day}/", header=True, inferSchema=True)
         write_to_postgres(df, "jdbc:postgresql://postgres-airflow:5432/airflow")
+
+
+        # df = spark.read.csv("s3a://refined/equipe", header=True, inferSchema=True)
+        # write_to_postgres(df, "jdbc:postgresql://postgres-airflow:5432/airflow")
 
     else:
         print("Unknown action:", action)
