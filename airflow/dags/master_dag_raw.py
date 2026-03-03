52eb65616d488e02af2e3b146d23c3e60364e2e9
Nour Guelmami <Nourelhouda.guelmami@wimbeetech.com>
2026-03-03 12:25:13 +0100

    last updates before mergeeeeeeeee

--- airflow/dags/master_dag_raw.py
+++ airflow/dags/master_dag_raw.py
@@ -1,6 +1,5 @@
 from airflow import DAG
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
-from airflow.operators.dummy import DummyOperator
 from datetime import datetime
 
 with DAG(
@@ -11,23 +10,20 @@ with DAG(
     tags=["MASTER", "ORCHESTRATION"],
 ) as dag:
 
-    start = DummyOperator(task_id="start")
-    end_ingest = DummyOperator(task_id="end_ingest")
-
-    # ingest_INS_sources = TriggerDagRunOperator(
-    #     task_id="trigger_ingest_sources",
-    #     trigger_dag_id="01-ING__ingest_all_ins_sources",
-    #     wait_for_completion=True,  # maintenant le master attend que ce DAG se termine
-    #     poke_interval=30,          # vérifie toutes les 60s si le DAG enfant est fini
-    # )
+    ingest_INS_sources = TriggerDagRunOperator(
+        task_id="trigger_ingest_sources",
+        trigger_dag_id="01-ING__ingest_all_ins_sources",
+        wait_for_completion=True,  # maintenant le master attend que ce DAG se termine
+        poke_interval=30,          # vérifie toutes les 60s si le DAG enfant est fini
+    )
     
 
-    # ingest_INS_dimensions = TriggerDagRunOperator(
-    #     task_id="trigger_ingest_dimensions",
-    #     trigger_dag_id="01-ING__ingest_all_ins_dimensions",
-    #     wait_for_completion=True,
-    #     poke_interval=30,
-    # )
+    ingest_INS_dimensions = TriggerDagRunOperator(
+        task_id="trigger_ingest_dimensions",
+        trigger_dag_id="01-ING__ingest_all_ins_dimensions",
+        wait_for_completion=True,
+        poke_interval=30,
+    )
 
     ingest_WorldBank_data = TriggerDagRunOperator(
         task_id="trigger_ingest_WorldBank_data",
@@ -43,21 +39,6 @@ with DAG(
         poke_interval=30,
     )
 
-    # ---------------------------
-    # DAG de transformation (SILVER)
-    # ---------------------------
-    trigger_transformations = TriggerDagRunOperator(
-        task_id="trigger_transformation_dags",
-        trigger_dag_id="00-MASTER__TRANSFORMED",  # DAG de transformations
-        wait_for_completion=False  # peut être False si tu veux juste lancer et ne pas bloquer
-    )
 
-    # ---------------------------
-    # ORCHESTRATION
-    # ---------------------------
-    start >> [
-        ingest_from_data_folder,
-        ingest_WorldBank_data
-        # ingest_INS_sources,
-        # ingest_INS_dimensions
-    ] >> end_ingest >> trigger_transformations
+    # ORCHESTRATION ORDER
+    ingest_from_data_folder >> ingest_WorldBank_data >> ingest_INS_sources >> ingest_INS_dimensions
